8329556e85bfd7b8f6867f037055c3d8fb1a8c1b
chandrakant1997 <ckpatel802@gmail.com>
2020-10-24 12:13:29 +0530

    Initial commit

---
+++ android/app/src/main/java/com/example/dropdown_with_api_ck/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.dropdown_with_api_ck;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,67 @@
+import 'dart:async';
+import 'dart:convert';
+
+import "package:flutter/material.dart";
+import 'package:http/http.dart' as http;
+
+void main() => runApp(MaterialApp(
+      title: "Hospital Management",
+      home: MyApp(),
+    ));
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String _mySelection;
+
+  final String url = "http://webmyls.com/php/getdata.php";
+  List data = List(); //edited line
+
+  Future<String> getSWData() async {
+    var res = await http
+        .get(Uri.encodeFull(url), headers: {"Accept": "application/json"});
+    var resBody = json.decode(res.body);
+
+    setState(() {
+      data = resBody;
+    });
+
+    print(resBody);
+
+    return "Sucess";
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    this.getSWData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: AppBar(
+        title: Text("Hospital Management"),
+      ),
+      body: new Center(
+        child: new DropdownButton(
+          items: data.map((item) {
+            return new DropdownMenuItem(
+              child: new Text(item['item_name']),
+              value: item['id'].toString(),
+            );
+          }).toList(),
+          onChanged: (newVal) {
+            setState(() {
+              _mySelection = newVal;
+            });
+          },
+          value: _mySelection,
+        ),
+      ),
+    );
+  }
+}
